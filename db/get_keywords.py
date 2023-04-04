4bb74864de274d0ab1aa3ed4219afe89a5e89664
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-04 21:12:48 +0300

    psycopg2 inserting keywords

--- db/get_keywords.py
+++ db/get_keywords.py
@@ -1,17 +1,19 @@
 import psycopg2
 from db.database import connect
-from config import host, password, user, name, port
+from config import load_config
+
+config = load_config('.env')
 
 
 def get_keywords_list():
     connect()
     keywords = []
     conn = psycopg2.connect(
-        host=host,
-        password=password,
-        user=user,
-        dbname=name,
-        port=port
+        host=config.db.host,
+        password=config.db.password,
+        user=config.db.user,
+        dbname=config.db.database,
+        port=config.db.port
     )
 
     cur = conn.cursor()
@@ -25,3 +27,14 @@ def get_keywords_list():
     return keywords
 
 
+def add_keyword_db(keyword):
+    conn = psycopg2.connect(
+        host=config.db.host,
+        password=config.db.password,
+        user=config.db.user,
+        dbname=config.db.database,
+        port=config.db.port
+    )
+    cur = conn.cursor()
+    query = f'INSERT INTO keywords (keyword) VALUES "{keyword}"'
+    cur.execute(query)
