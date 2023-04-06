4a5c54321f5e5788119dab38733ce1480b02f6d8
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-07 00:40:09 +0300

    output keyword, was telegram_id

--- models/keywords.py
+++ models/keywords.py
@@ -1,6 +1,34 @@
+from sqlalchemy import BigInteger, Column, String, select, insert
+from sqlalchemy.orm import sessionmaker
+
 from db.db_base import Base
 
 
 class Keyword(Base):
     __tablename__ = 'keywords'
-    user_id =
+    telegram_id = Column(BigInteger, primary_key=True)
+    keyword = Column(String(length=100))
+
+    @classmethod
+    async def get_keyword(cls, session_maker: sessionmaker, telegram_id: int):
+        async with session_maker() as db_session:
+            sql = select(cls.keyword).where(cls.telegram_id == telegram_id)
+            request = await db_session.execute(sql)
+            keyword: cls = request.scalar()
+        return keyword
+
+    @classmethod
+    async def add_keyword(cls,
+                          session_maker: sessionmaker,
+                          telegram_id: int,
+                          keyword: str):
+
+        async with session_maker() as db_session:
+            sql = insert(cls).values(
+                telegram_id=telegram_id,
+                keyword=keyword
+            ).returning('*')
+
+            result = await db_session.execute(sql)
+            await db_session.commit()
+            return result.first()
