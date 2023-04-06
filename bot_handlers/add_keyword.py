9cd7fabd59bb880444caccaac06997db6a136ac3
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-07 00:35:12 +0300

    release add keyword function

--- bot_handlers/add_keyword.py
+++ bot_handlers/add_keyword.py
@@ -2,13 +2,17 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 
 from keyboards.reply.main_menu import back_main_menu
+from models.keywords import Keyword
 from states.keyword_state import KeywordState
 
 
 async def add_keyword(message: types.Message, state: FSMContext):
     await state.update_data(keyword=message.text)
     data = await state.get_data()
+    word = f"{data['keyword']}"
+    session_maker = message.bot.get('db')
     await message.answer(f"Ваше ключевое слово: {data['keyword']}. Я запомнил.", reply_markup=back_main_menu)
+    await Keyword.add_keyword(session_maker=session_maker, telegram_id=message.from_user.id, keyword=word)
     await state.finish()
 
 
