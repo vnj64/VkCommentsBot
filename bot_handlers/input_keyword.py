7ffb5ee1646ac26a0920b607f0e20a8c20809374
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-09 22:25:36 +0300

    get keywords in 1 line

--- bot_handlers/input_keyword.py
+++ bot_handlers/input_keyword.py
@@ -1,11 +1,13 @@
 from aiogram import types, Dispatcher
+
+from keyboards.reply.main_menu import back_main_menu
 from states.keyword_state import KeywordState
 
 
 async def get_keyword(message: types.Message):
-    await message.answer("Введите ключевое слово: ")
+    await message.answer("Введите ключевые слова через пробел: ", reply_markup=back_main_menu)
     await KeywordState.keyword.set()
 
 
 def register_get_keyword(dp: Dispatcher):
-    dp.register_message_handler(get_keyword, text='Добавить ключевое слово 📔')
+    dp.register_message_handler(get_keyword, text='Добавить ключевое слово 📔', state='*')
