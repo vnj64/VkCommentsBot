ec9722203982ee37ccf9be5bc70f22aaae874c9b
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-09 22:19:51 +0300

    returning comments func

--- api_handlers/checker.py
+++ api_handlers/checker.py
@@ -1,17 +1,20 @@
-import asyncio
-
-import aioreq
-import requests
 from config import load_config
+import requests
 
 config = load_config('.env')
 
 
-async def checker(group_id, post_id, count):
-    async with aioreq.Client() as client:
-        response = await client.get(f"https://api.vk.com/method/wall.getComments?owner_id={group_id}&post_id={post_id}"
-                                    f"&count={count}&sort=asc&access_token={config.api.api_token}&v=5.131")
+def comments_getter(group_id, post_id):
+    comments = []
+    try:
+        req = requests.get(f"https://api.vk.com/method/wall.getComments?owner_id={group_id}&post_id={post_id}"
+                           f"&count=10&sort=asc&access_token={config.api.api_token}&v=5.131")
+        json_data = req.json()
+        resp = json_data['response']['items']
+        for i in resp:
+            comments.append([i['text'], i['from_id']])
 
-        print(response.content)
+    except Exception as e:
+        return e
 
-asyncio.run(checker("-185709218", "63", "100"))
+    return comments
