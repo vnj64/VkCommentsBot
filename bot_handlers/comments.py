d61e89d8fc03db5c5b3336d77943e0b213c01a0a
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-09 22:24:15 +0300

    added new button

--- bot_handlers/comments.py
+++ bot_handlers/comments.py
@@ -3,9 +3,9 @@ from aiogram import types, Dispatcher
 from keyboards.reply.comments import comments_variables
 
 
-async def check_comments(message: types.Message):
-    await message.answer("Выберите действие: ", reply_markup=comments_variables)
+async def check_variables(message: types.Message):
+    await message.answer("Что хотите сделать?", reply_markup=comments_variables)
 
 
-def register_comments(dp: Dispatcher):
-    dp.register_message_handler(check_comments, text="Комментарии 📓")
+def register_variables_handler(dp: Dispatcher):
+    dp.register_message_handler(check_variables, text="Комментарии 📓")
