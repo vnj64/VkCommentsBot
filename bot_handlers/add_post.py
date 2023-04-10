c45ef9d29c7bbd51c02ff6a84e0b6768a4202654
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-10 19:58:21 +0300

    adding post_info in DB

--- bot_handlers/add_post.py
+++ bot_handlers/add_post.py
@@ -1,8 +1,10 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
+from sqlalchemy.orm import sessionmaker
 
 from keyboards.reply.main_menu import back_main_menu
 from states.comment_state import CommentState
+from models.post import Post
 
 from api_handlers.checker import comments_getter
 
@@ -21,7 +23,11 @@ async def return_group_id(message: types.Message, state: FSMContext):
 async def all_rdy(message: types.Message, state: FSMContext):
     await state.update_data(post=message.text)
     data = await state.get_data()
+    session_maker = message.bot.get('db')
+
     await message.answer(comments_getter(f"{data['group']}", f"{data['post']}"), reply_markup=back_main_menu)
+    await Post.add_post_info(session_maker=session_maker, telegram_id=message.from_user.id,
+                             owner_id=data['group'], post_id=data['post'])
     await state.finish()
 
 
