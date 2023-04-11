3fae563be60e5b663917f4e9a0325141380167ee
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-11 23:13:25 +0300

    access token db model

---
+++ models/ac_token.py
@@ -0,0 +1,30 @@
+from db.db_base import Base
+
+from sqlalchemy import BigInteger, Column, String, select, insert, Integer
+from sqlalchemy.orm import sessionmaker
+
+
+class Token(Base):
+    __tablename__ = 'encoded_token'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    telegram_id = Column(BigInteger)
+    token = Column(String)
+
+    @classmethod
+    async def get_token(cls, session_maker: sessionmaker, telegram_id: int):
+        async with session_maker() as db_session:
+            sql = select(cls.token).where(cls.telegram_id == telegram_id)
+            request = await db_session.execute(sql)
+            token: cls = request.all()
+        return token
+
+    @classmethod
+    async def add_token(cls,
+                        session_maker: sessionmaker,
+                        telegram_id: int,
+                        token: str):
+        async with session_maker() as db_session:
+            sql = insert(cls).values(
+                telegram_id=telegram_id,
+                token=
+            )
