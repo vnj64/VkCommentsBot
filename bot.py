99a482cce93870057a295a7418b755e4abca2d2c
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-04-12 21:30:26 +0300

    registered new handlers

--- bot.py
+++ bot.py
@@ -6,12 +6,14 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from middlewares.db import DbMiddleware
 from bot_handlers.start import register_start
 from bot_handlers.profile import register_profile
-from bot_handlers.input_keyword import register_get_keyword
-from bot_handlers.add_keyword import register_add_keyword
-from bot_handlers.get_keywords import register_checker
-from bot_handlers.comments import register_variables_handler
-from bot_handlers.add_post import register_group_add
-from bot_handlers.get_comments import register_get_comments
+from bot_handlers.keyword.input_keyword import register_get_keyword
+from bot_handlers.keyword.add_keyword import register_add_keyword
+from bot_handlers.keyword.get_keywords import register_checker
+from bot_handlers.comments.comments import register_variables_handler
+from bot_handlers.post.add_post import register_group_add
+from bot_handlers.comments.get_comments import register_get_comments
+from bot_handlers.token.add_token import register_add_token
+from bot_handlers.token.delete_token import register_delete_token
 from config import load_config
 from db.tg_db import create_db_session
 
@@ -31,6 +33,8 @@ def register_all_handlers(dp):
     register_variables_handler(dp)
     register_group_add(dp)
     register_get_comments(dp)
+    register_add_token(dp)
+    register_delete_token(dp)
 
 
 async def main():
